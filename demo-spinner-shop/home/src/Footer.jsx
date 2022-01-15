7995f7f5dac6a7cb1ad7b763490e918e7b8f3ac3
alvinend <alvinendratno@live.com>
2022-01-15 05:11:23 +0000

    Implement Footer and Header at Home

---
+++ demo-spinner-shop/home/src/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className="pg-5 bg-blue-500 text-white text-3xl font-bold">
+      Only The best Spinnner
+    </div>
+  )
+}
+
+export default Footer
