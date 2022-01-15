0d817bb9cc873c8854486f6507eb45b8710a20a5
alvinend <alvinendratno@live.com>
2022-01-15 05:16:11 +0000

    Using Exposed Component on PDP

--- demo-spinner-shop/pdp/src/App.jsx
+++ demo-spinner-shop/pdp/src/App.jsx
@@ -1,14 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+import Header from 'home/Header'
+import Footer from 'home/Footer'
+
 import "./index.scss";
 
 const App = () => (
   <div className="mt-10 text-3xl mx-auto max-w-6xl">
-    <div>Name: pdp</div>
+    <Header />
     <div>Framework: react</div>
     <div>Language: JavaScript</div>
-    <div>CSS: Tailwind</div>
+    <Footer />
   </div>
 );
 ReactDOM.render(<App />, document.getElementById("app"));
